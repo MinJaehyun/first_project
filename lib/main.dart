242dd89a1acc7693548794e3449b02274b76ab63
{krism01} <{krism01@naver.com}>
2022-12-07 05:03:57 +0900

    상중하 분리하고 각각에 아이콘 정렬되게 배치

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,21 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       // home: Text('안녕'),
-      home: Icon(Icons.zoom_in),
+      // home: Icon(Icons.zoom_in),
+      home: Scaffold(
+        appBar: AppBar(title: Text('앱 제목')),
+
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          // crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [Icon(Icons.star), Icon(Icons.star), Icon(Icons.star)],
+        ),
+
+        bottomNavigationBar: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [Icon(Icons.phone),Icon(Icons.message),Icon(Icons.contact_page)],
+        ),
+      )
     );
   }
 }
