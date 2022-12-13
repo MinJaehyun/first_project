c5022a895c1023267e6057881dc15998dc6f748c
{krism01} <{krism01@naver.com}>
2022-12-13 11:23:36 +0900

    use flutterToast
    0. 기본 구조 생성
    1. flutterToast library 사용
    2. 예제 copy 및 custum 사용
    3. btn 클릭 시, 팝업창 생성

---
+++ lib/main7.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '19-2.Toast Msg',
+      theme: ThemeData.light(),
+      debugShowCheckedModeBanner: false,
+      home: MyPage(),
+    );
+  }
+}
+
+class MyPage extends StatelessWidget {
+  const MyPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Toast Message'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: (){
+            // 클릭 시, 하단 팝업 띄우기
+            flutterToast();
+          },
+          child: Text('Toast Btn'),
+        ),
+      ),
+    );
+  }
+}
+
+void flutterToast() {
+  Fluttertoast.showToast(
+      msg: "This is Center Short Toast",
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.BOTTOM,
+      timeInSecForIosWeb: 1,
+      backgroundColor: Colors.redAccent,
+      textColor: Colors.white,
+      fontSize: 30.0
+  );
+}
