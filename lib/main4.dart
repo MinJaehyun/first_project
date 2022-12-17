9ba84d45923df4ca6c88e3e9772a08e0e2993d69
{krism01} <{krism01@naver.com}>
2022-12-17 16:46:02 +0900

    코드 정리

--- lib/main4.dart
+++ lib/main4.dart
@@ -21,21 +21,14 @@ class Grade extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.amber[200],
-      appBar: AppBar(
-        backgroundColor: Colors.amber[800],
-        centerTitle: true,
-        title: const Text('PULDA'),
-      ),
-      
-      body: const ContextBody(),
-      
-      bottomNavigationBar: Row(
-        children: const [
-          Icon(Icons.keyboard_backspace), Icon(Icons.circle), Icon(Icons.stop)
-        ],
-      ),
-    );
+        backgroundColor: Colors.amber[200],
+        appBar: AppBar(
+          backgroundColor: Colors.amber[800],
+          centerTitle: true,
+          title: const Text('PULDA'),
+        ),
+        body: const ContextBody(),
+        bottomNavigationBar: BottomAppBar());
   }
 }
 
@@ -45,65 +38,67 @@ class ContextBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.all(30),
-      margin: const EdgeInsets.all(20),
-
-
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          // logo
-
-           Center(
-             // NOTE: CircleAvatar
-            child: CircleAvatar(
-              backgroundImage: const AssetImage('assets/camera1.jpg'),
-              radius: 60.0,
-              backgroundColor: Colors.amber[200],
+        padding: const EdgeInsets.all(30),
+        margin: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              // NOTE: CircleAvatar
+              child: CircleAvatar(
+                backgroundImage: const AssetImage('assets/camera1.jpg'),
+                radius: 60.0,
+                backgroundColor: Colors.amber[200],
+              ),
             ),
-          ),
-          // NOTE: 구분선
-          const Divider(
-            height: 60,
-            indent: 50,
-            endIndent: 100,
-            thickness: 2.5,
-          ),
-          const Text('NAME', style: TextStyle(color: Colors.white)),
-          const SizedBox(height: 10),
-          const Text('Min JH', style: TextStyle(color: Colors.white, fontWeight: FontWeight.w400, fontSize: 50, letterSpacing: 5)),
-          const SizedBox(height: 10),
-          Column(
-            children:  [
-              Row(
-                children: const [
-                  Icon(Icons.check_circle_outline, color: Colors.blueGrey),
-                  SizedBox(width: 10.0),
-                  Text('using lightsaber', style: TextStyle(color: Colors.blueGrey)),
-                ],
-              ),
-              const SizedBox(height: 5),
-              Row(
-                children: const [
-                  Icon(Icons.check_circle_outline, color: Colors.blueGrey),
-                  SizedBox(width: 10.0),
-                  Text('hero face tattoo', style: TextStyle(color: Colors.blueGrey)),
-                ],
-              ),
-              const SizedBox(height: 5),
-              Row(
-                children: const [
-                  Icon(Icons.check_circle_outline, color: Colors.blueGrey),
-                  SizedBox(width: 10.0),
-                  Text('fire flames', style: TextStyle(color: Colors.blueGrey)),
-                ],
-              ),
-              const SizedBox(height: 5),
-            ],
-          ),
-        ],
-      )
-
-    );
+            // NOTE: 구분선
+            const Divider(
+              height: 60,
+              indent: 50,
+              endIndent: 50,
+              thickness: 2.5,
+            ),
+            const Text('NAME', style: TextStyle(color: Colors.white)),
+            const SizedBox(height: 10),
+            const Text('Min JH',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 50,
+                    letterSpacing: 5)),
+            const SizedBox(height: 10),
+            Column(
+              children: [
+                Row(
+                  children: const [
+                    Icon(Icons.check_circle_outline, color: Colors.blueGrey),
+                    SizedBox(width: 10.0),
+                    Text('using lightsaber',
+                        style: TextStyle(color: Colors.blueGrey)),
+                  ],
+                ),
+                const SizedBox(height: 5),
+                Row(
+                  children: const [
+                    Icon(Icons.check_circle_outline, color: Colors.blueGrey),
+                    SizedBox(width: 10.0),
+                    Text('hero face tattoo',
+                        style: TextStyle(color: Colors.blueGrey)),
+                  ],
+                ),
+                const SizedBox(height: 5),
+                Row(
+                  children: const [
+                    Icon(Icons.check_circle_outline, color: Colors.blueGrey),
+                    SizedBox(width: 10.0),
+                    Text('fire flames',
+                        style: TextStyle(color: Colors.blueGrey)),
+                  ],
+                ),
+                const SizedBox(height: 5),
+              ],
+            ),
+          ],
+        ));
   }
 }
