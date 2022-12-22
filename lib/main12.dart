c6100cd47b88ea95f3f396ed8c354eea3ebc57e5
{krism01} <{krism01@naver.com}>
2022-12-22 18:55:10 +0900

    test

---
+++ lib/main12.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart'; // 'pf'
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyPage(),
+    );
+  }
+}
+
+class MyPage extends StatefulWidget {
+  const MyPage({Key? key}) : super(key: key);
+
+  @override
+  State<MyPage> createState() => _MyPageState();
+}
+
+class _MyPageState extends State<MyPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: null,
+      bottomNavigationBar: BottomAppBar(),
+    );
+  }
+}
