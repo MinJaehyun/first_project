38190de68b236265307d2b8b987c7b7128ec7927
{krism01} <{krism01@naver.com}>
2023-01-10 21:22:50 +0900

    test signup

--- lib/main24/chat_screen/chat_screen.dart
+++ lib/main24/chat_screen/chat_screen.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatefulWidget {
@@ -8,6 +9,24 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  final _authentication = FirebaseAuth.instance;
+  User? loggerUser;
+
+  @override
+  void initState() {
+    super.initState();
+    getCurrentUser();
+  }
+
+  void getCurrentUser() {
+    try{
+      final user = _authentication.currentUser;
+      print(user);
+    }catch(e){
+      print(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
