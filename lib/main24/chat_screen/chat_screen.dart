88c214ef9f62bc1b788eeb9556d97d33222bfd71
{krism01} <{krism01@naver.com}>
2023-01-11 13:23:34 +0900

    서버데이터 가져오는 동안 로딩화면 보여주고, 서버에 입력된 내용을 화면에 띄움

--- lib/main24/chat_screen/chat_screen.dart
+++ lib/main24/chat_screen/chat_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -35,25 +36,44 @@ class _ChatScreenState extends State<ChatScreen> {
         title: const Text('Chat sreen'),
         centerTitle: true,
         actions: [
-          IconButton(onPressed: () async {
-            // 회원탈퇴
-            // FirebaseAuth.instance.currentUser!.delete();
-            // TODO: user 제거 및 login page로 돌아가기
-            await FirebaseAuth.instance.signOut().then((result) => {
-              // print("test then result")
-            });
-            Navigator.pop(context);
-            // NOTE: 제일 처음으로 돌아가기
-            // Navigator.of(context).popUntil((route) => route.isFirst);
-          }, icon: const Icon(Icons.logout)),
+          IconButton(
+              onPressed: () async {
+                await FirebaseAuth.instance.signOut().then((result) =>
+                {
+                  // print("test then result")
+                });
+                Navigator.pop(context);
+                // NOTE 제일 처음으로 돌아가기: Navigator.of(context).popUntil((route) => route.isFirst);
+                // NOTE 회원탈퇴: FirebaseAuth.instance.currentUser!.delete();
+              },
+              icon: const Icon(Icons.logout)),
         ],
       ),
-      body: const Center(
-        child: Text(
-          'chat screen',
-          style: TextStyle(fontSize: 33),
-        ),
-      ),
+      body: StreamBuilder(
+          stream: FirebaseFirestore.instance.collection(
+              '/chats/pZaVfyO0YlsbF0VMyTLL/messages').snapshots(),
+          builder: (context, AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+            // 우선 처리해야 한다
+            if(snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            final docs = snapshot.data!.docs;
+            return ListView.builder(
+              itemCount: docs.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    docs[index]['text'],
+                    style: TextStyle(fontSize: 33),
+                  ),
+                );
+              },
+
+            );
+          }),
     );
   }
 }
