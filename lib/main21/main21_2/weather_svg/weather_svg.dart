867eccbed8600f40a0e9d38b8520084c4a3c4fec
{krism01} <{krism01@naver.com}>
2023-01-21 14:23:18 +0900

    개선 1. 중복 코드 개선 2. Run 실행 시, 초기화 실패 에러 뜬다 - 핫리로드 실행 시, 실행 된다 -- 뭐가 문제???

--- lib/main21/main21_2/weather_svg/weather_svg.dart
+++ lib/main21/main21_2/weather_svg/weather_svg.dart
@@ -2,52 +2,43 @@ import 'package:flutter_svg/flutter_svg.dart';
 
 class Model {
   final int conditionId;
-
   Model(this.conditionId);
 
-  SvgPicture getSvg(conditionId) {
+  SvgPicture getSvg() {
+    SvgPicture duplicationCode({file}) {
+      return SvgPicture.asset(
+        'assets/main21/svg/$file.svg',
+        width: 100,
+        height: 100,
+      );
+    }
+
     if (conditionId >= 200 && conditionId < 300) {
-      return SvgPicture.asset(
-        'assets/main21/svg/lightning.svg',
-        width: 100,
-        height: 100,
-      );
+      String file = 'lightning';
+      return duplicationCode(file: file);
+      // return SvgPicture.asset(
+      //   'assets/main21/svg/lightning.svg',
+      //   width: 100,
+      //   height: 100,
+      // );
     } else if (conditionId >= 300 && conditionId < 500) {
-      return SvgPicture.asset(
-        'assets/main21/svg/rain.svg',
-        width: 100,
-        height: 100,
-      );
+      String file = 'rain';
+      return duplicationCode(file: file);
     } else if (conditionId >= 600 && conditionId < 700) {
-      return SvgPicture.asset(
-        'assets/main21/svg/snow.svg',
-        width: 100,
-        height: 100,
-      );
+      String file = 'snow';
+      return duplicationCode(file: file);
     } else if (conditionId >= 700 && conditionId < 800) {
-      return SvgPicture.asset(
-        'assets/main21/svg/smog.svg',
-        width: 100,
-        height: 100,
-      );
+      String file = 'smog';
+      return duplicationCode(file: file);
     } else if (conditionId == 800) {
-      return SvgPicture.asset(
-        'assets/main21/svg/sun.svg',
-        width: 100,
-        height: 100,
-      );
+      String file = 'sun';
+      return duplicationCode(file: file);
     } else if (conditionId >= 801) {
-      return SvgPicture.asset(
-        'assets/main21/svg/cloud_sun.svg',
-        width: 100,
-        height: 100,
-      );
+      String file = 'cloud_sun';
+      return duplicationCode(file: file);
     } else {
-      return SvgPicture.asset(
-        'assets/main21/svg/icon.svg',
-        width: 100,
-        height: 100,
-      );
+      String file = 'icon';
+      return duplicationCode(file: file);
     }
   }
 }
