6179bc3d0f18e3779c36f0d762ba41bb3cd69f13
{krism01} <{krism01@naver.com}>
2023-01-21 17:45:59 +0900

    test getter

--- lib/main21/main21_2/geolocation/geolocation.dart
+++ lib/main21/main21_2/geolocation/geolocation.dart
@@ -2,16 +2,22 @@ import 'package:geolocator/geolocator.dart';
 
 // note: 위도, 경도 받기 위한 함수
 class GetLocation {
-  late double latitude2;
-  late double longitude2;
+  // late double latitude2;
+  // late double longitude2;
+  late Position position;
 
   Future<dynamic> getLocation() async {
     await Geolocator.requestPermission();
-    Position position = await Geolocator.getCurrentPosition(
+    position = await Geolocator.getCurrentPosition(
         desiredAccuracy: LocationAccuracy.high);
-
-    latitude2 = position.latitude;
-    longitude2 = position.longitude;
+    // latitude2 = position.latitude;
+    // longitude2 = position.longitude;
     return position;
+  }
+  double get latitude {
+    return position.latitude;
+  }
+  double get longitude {
+    return position.longitude;
   }
 }
