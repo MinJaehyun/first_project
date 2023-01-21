a98f44d61ffa05a98e901fefed1a667315e8b5bf
{krism01} <{krism01@naver.com}>
2023-01-21 13:49:15 +0900

    개선 1. import 명칭 통일 2. 불필요한 변수 제거 및 변수 끼리 모아둠 3. 불필요하게 다른 클래스에 메서드에 인자를 넣는 과정 제거 4. body 코드 분리 및 화면 간 데이터 전달

--- lib/main21/main21_2/my_page/my_page.dart
+++ lib/main21/main21_2/my_page/my_page.dart
@@ -1,13 +1,11 @@
-import 'package:first_project/main21/main21_2/air_pollution/air_pollution.dart';
-import 'package:first_project/main21/main21_2/network/network.dart';
 import 'package:flutter/material.dart';
+import 'package:first_project/main21/main21_2/air_pollution/air_pollution.dart';
+import 'package:first_project/main21/main21_2/weather_svg/weather_svg.dart';
+import 'package:first_project/main21/main21_2/geolocation/geolocation.dart';
+import 'package:first_project/main21/main21_2/network/network.dart';
+import 'package:first_project/main21/main21_2/body/body.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:google_fonts/google_fonts.dart';
-import '../geolocation/geolocation.dart';
-import '../weather_svg/weather_svg.dart';
-import 'package:timer_builder/timer_builder.dart';
-import 'package:intl/intl.dart';
 
 class MyPage extends StatefulWidget {
   const MyPage({Key? key}) : super(key: key);
@@ -17,58 +15,53 @@ class MyPage extends StatefulWidget {
 }
 
 class _MyPageState extends State<MyPage> {
-  late double latitude3, longitude3;
-  late String url;
   String apiKey = dotenv.get('API_KEY');
-  dynamic weatherOjb2;
-  late int conditionId;
-  late SvgPicture loadSvg2;
-  late double temperature;
-  late int index;
-  late Widget indexImg;
-  late Widget indexText;
-  late double particulateMatter;
-  late double ultraParticulateMatter;
+  late double latitude3,
+      longitude3,
+      temperature,
+      particulateMatter,
+      ultraParticulateMatter;
+  late SvgPicture loadSvg;
+  late Widget indexImg, indexText;
 
   Future<void> getWeather() async {
-    GetLocation getLocation = GetLocation();
-    await getLocation.getLocation();
-    latitude3 = getLocation.latitude2;
-    longitude3 = getLocation.longitude2;
+    try {
+      GetLocation getLocation = GetLocation();
+      await getLocation.getLocation();
+      latitude3 = getLocation.latitude2;
+      longitude3 = getLocation.longitude2;
 
-    url =
-        "https://api.openweathermap.org/data/2.5/weather?lat=$latitude3&lon=$longitude3&appid=$apiKey&units=metric";
-    print(url);
-    Network getJsonData = Network(url);
-    var weatherOjb = await getJsonData.getJsonData();
-    weatherOjb2 = weatherOjb;
-    conditionId = weatherOjb2['weather'][0]['id'];
-    temperature = weatherOjb2['main']['temp'];
-    // print(conditionId);
+      String url = "https://api.openweathermap.org/data/2.5/weather?lat=$latitude3&lon=$longitude3&appid=$apiKey&units=metric";
 
-    // todo: Model 에 int 넘기기
-    Model model = Model(conditionId);
-    SvgPicture loadSvg = model.getSvg(conditionId);
-    loadSvg2 = loadSvg;
-  }
+      Network getJsonData = Network(url);
+      var weatherOjb = await getJsonData.getJsonData();
+      int conditionId = weatherOjb['weather'][0]['id'];
+      temperature = weatherOjb['main']['temp'];
 
-  String getTime() {
-    var time = DateTime.now();
-    return DateFormat(' EEEE \n d MMM, yyy \n\n hh:mm:ss a').format(time);
+      Model model = Model(conditionId);
+      loadSvg = model.getSvg();
+    } catch (e) {
+      debugPrint('getWeather err');
+    }
   }
 
   Future<void> getAirPollution() async {
-    var airUrl = 'http://api.openweathermap.org/data/2.5/air_pollution?lat=$latitude3&lon=$longitude3&appid=$apiKey';
-    Network getJsonData = Network(airUrl);
-    var airPollutionOjb = await getJsonData.getJsonData();
-    // print(airPollutionOjb);
-    index = airPollutionOjb['list'][0]['main']['aqi'];
-    print(index);
-    particulateMatter = airPollutionOjb['list'][0]['components']['pm10'];
-    ultraParticulateMatter = airPollutionOjb['list'][0]['components']['pm2_5'];
-    AirPollution airPollution = AirPollution(index);
-    indexImg = airPollution.getAqiImg(index);
-    indexText = airPollution.getAqiText(index);
+    try {
+      String airUrl = "http://api.openweathermap.org/data/2.5/air_pollution?lat=$latitude3&lon=$longitude3&appid=$apiKey";
+
+      Network getJsonData = Network(airUrl);
+      var airPollutionOjb = await getJsonData.getJsonData();
+      int index = airPollutionOjb['list'][0]['main']['aqi'];
+      particulateMatter = airPollutionOjb['list'][0]['components']['pm10'];
+      ultraParticulateMatter =
+          airPollutionOjb['list'][0]['components']['pm2_5'];
+
+      AirPollution airPollution = AirPollution(index);
+      indexImg = airPollution.getAqiImg();
+      indexText = airPollution.getAqiText();
+    } catch (e) {
+      debugPrint('getAirPollution err');
+    }
   }
 
   void getFunc() async {
@@ -85,101 +78,24 @@ class _MyPageState extends State<MyPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.orangeAccent,
-        elevation: 0,
-        leading: IconButton(
-          onPressed: () {},
-          // note: https://material.io/tools/icons
-          icon: const Icon(Icons.near_me),
-        ),
-        actions: [
-          IconButton(onPressed: () {}, icon: const Icon(Icons.my_location)),
-        ],
-      ),
-      body: Container(
-        decoration: const BoxDecoration(
-          color: Colors.orangeAccent,
-        ),
-        child: FutureBuilder(
-          future: Future.delayed(
-            const Duration(seconds: 2),
+        appBar: AppBar(
+          backgroundColor: Colors.orangeAccent,
+          elevation: 0,
+          leading: IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.near_me),
           ),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-            return SingleChildScrollView(
-              child: Center(
-                child: Column(
-                  children: [
-                    const SizedBox(height: 100),
-                    loadSvg2,
-                    const SizedBox(height: 5),
-                    Text(
-                      '${temperature.round().toString()} \u2103',
-                      style: GoogleFonts.getFont('Lato', fontSize: 25,color: Colors.white)
-                    ),
-                    const SizedBox(height: 70),
-                    TimerBuilder.periodic(
-                      const Duration(seconds: 1),
-                      builder: (context) {
-                        return Text(
-                          getTime(),
-                          style: const TextStyle(color: Colors.white, fontSize: 20),
-                        );
-                      },
-                    ),
-                    const SizedBox(height: 20),
-                    const Divider(thickness: 1, color: Colors.white70),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Column(
-                            children: [
-                              Text('AQI(대기질 지수)', style: GoogleFonts.getFont('Lato', fontSize: 15,color: Colors.white)),
-                              const SizedBox(height: 10),
-                              // Image.asset('assets/main21/pollution_image/bad.png', width: 50),
-                              indexImg,
-                              const SizedBox(height: 10),
-                              // Text("매우나쁨", style: GoogleFonts.getFont('Lato', fontSize: 15,color: Colors.white)),
-                              indexText,
-                            ],
-                          ),
-                          const SizedBox(width: 5,),
-                          Column(
-                            children: [
-                              Text('미세먼지', style: GoogleFonts.getFont('Lato', fontSize: 15,color: Colors.white)),
-                              const SizedBox(height: 10),
-                              Text('$particulateMatter', style: GoogleFonts.getFont('Lato', fontSize: 38,color: Colors.white)),
-                              const SizedBox(height: 10),
-                              Text("㎍/㎥", style: GoogleFonts.getFont('Lato', fontSize: 15,color: Colors.white)),
-                            ],
-                          ),
-                          const SizedBox(width: 5,),
-                          Column(
-                            children: [
-                              Text('초미세먼지', style: GoogleFonts.getFont('Lato', fontSize: 15,color: Colors.white)),
-                              const SizedBox(height: 10),
-                              Text('$ultraParticulateMatter', style: GoogleFonts.getFont('Lato', fontSize: 38,color: Colors.white)),
-                              const SizedBox(height: 10),
-                              Text("㎍/㎥", style: GoogleFonts.getFont('Lato', fontSize: 15,color: Colors.white)),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            );
-          },
+          actions: [
+            IconButton(onPressed: () {}, icon: const Icon(Icons.my_location)),
+          ],
         ),
-      ),
-    );
+        body: DustBody(
+          loadSvg: loadSvg,
+          temperature: temperature,
+          indexImg: indexImg,
+          indexText: indexText,
+          particulateMatter: particulateMatter,
+          ultraParticulateMatter: ultraParticulateMatter,
+        ));
   }
 }
