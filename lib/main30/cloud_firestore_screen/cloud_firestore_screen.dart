ff84196ba21512b9f41bc487fab5037340740b15
{krism01} <{krism01@naver.com}>
2023-01-31 22:09:35 +0900

    DB 저장하기 위해 팝업창 구현

--- lib/main30/cloud_firestore_screen/cloud_firestore_screen.dart
+++ lib/main30/cloud_firestore_screen/cloud_firestore_screen.dart
@@ -1,6 +1,7 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:first_project/main30/screen/product_edit.dart';
 import 'package:flutter/material.dart';
+import 'package:first_project/main30/screen/product_crud.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class CloudFirestoreScreen extends StatefulWidget {
   const CloudFirestoreScreen({Key? key}) : super(key: key);
@@ -10,27 +11,104 @@ class CloudFirestoreScreen extends StatefulWidget {
 }
 
 class _CloudFirestoreScreenState extends State<CloudFirestoreScreen> {
+  final TextEditingController titleController = TextEditingController();
+  final TextEditingController subTitleController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Cloud Firestore'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              // note: DB 저장하기 위해 팝업창 구현
+              showDialog(
+                context: context,
+                builder: (context) {
+                  return Dialog(
+                    backgroundColor: Colors.white,
+                    child: Padding(
+                      padding: const EdgeInsets.all(20),
+                      child: Column(
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text('title'),
+                              TextField(
+                                controller: titleController,
+                                decoration: const InputDecoration(
+                                    enabledBorder: UnderlineInputBorder(
+                                        borderSide:
+                                            BorderSide(color: Colors.grey))),
+                                style: const TextStyle(color: Colors.black),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 25),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text('subTitle'),
+                              TextField(
+                                controller: subTitleController,
+                                decoration: const InputDecoration(
+                                    enabledBorder: UnderlineInputBorder(
+                                        borderSide:
+                                            BorderSide(color: Colors.grey))),
+                                style: const TextStyle(color: Colors.black),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 25),
+                          ElevatedButton(
+                            onPressed: () async {
+                              var title = titleController.text;
+                              var subTitle = subTitleController.text;
+                              await FirebaseFirestore.instance.collection('posts').add({ 'title': title, 'body': subTitle, 'timeStamp': Timestamp.now() });
+                              titleController.text = '';
+                              subTitleController.text = '';
+                              if (!mounted) return;
+                              Navigator.of(context).pop();
+                            },
+                            child: const Text('Create'),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              );
+            },
+            icon: const Icon(Icons.add),
+          ),
+          IconButton(
+            onPressed: () {
+              // note: 로그아웃
+              FirebaseAuth.instance.signOut();
+              Navigator.of(context).pop();
+            },
+            icon: const Icon(Icons.logout),
+          ),
+        ],
       ),
       // note: 전체 목록
       body: StreamBuilder(
-          stream: FirebaseFirestore.instance.collection('posts').snapshots(),
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return ListView.builder(
-                itemCount: snapshot.data!.docs.length,
-                itemBuilder: (context, index) {
-                  var documentSnapshot = snapshot.data!.docs[index];
-                  return ProductEdit(documentSnapshot: documentSnapshot);
-                },
-              );
-            }
-            return const Center(child: CircularProgressIndicator());
-          }),
+        stream: FirebaseFirestore.instance.collection('posts').orderBy('timeStamp', descending: true).snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              itemCount: snapshot.data!.docs.length,
+              itemBuilder: (context, index) {
+                var documentSnapshot = snapshot.data!.docs[index];
+                return ProductEdit(documentSnapshot: documentSnapshot);
+              },
+            );
+          }
+          return const Center(child: CircularProgressIndicator());
+        },
+      ),
     );
   }
 }
