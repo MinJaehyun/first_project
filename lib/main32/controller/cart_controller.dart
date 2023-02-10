8e7be745822648a39ee1509d65dfc7eaaedafc1f
{krism01} <{krism01@naver.com}>
2023-02-10 22:41:37 +0900

    cart controller

---
+++ lib/main32/controller/cart_controller.dart
@@ -0,0 +1,12 @@
+import 'package:first_project/main32/models/product.dart';
+import 'package:get/get.dart';
+
+// 장바구니에 담긴 상품의 가격의 합을 구하는 controller
+class CartController extends GetxController {
+  var cartList = <Product>[].obs;
+  double get totalPrice => cartList.fold(0, (e, item) => e + item.price);
+
+  void addToItem(Product product) {
+    cartList.add(product);
+  }
+}
