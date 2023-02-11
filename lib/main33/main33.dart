4f6c9d2ae9393008b2e3456ad0f3974622dc813a
{krism01} <{krism01@naver.com}>
2023-02-11 15:14:01 +0900

    main

---
+++ lib/main33/main33.dart
@@ -0,0 +1,18 @@
+import 'package:first_project/main33/view/product_page.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      home: ProductPage(),
+    );
+  }
+}
