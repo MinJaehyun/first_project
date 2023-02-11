68734646572c4e9676c17b0b5a2c29aef1f5cf1d
{krism01} <{krism01@naver.com}>
2023-02-11 18:36:28 +0900

    GetX 설정하고 Obx 사용하여 관련 내용 가져올 준비 완료

--- lib/main33/view/product_page.dart
+++ lib/main33/view/product_page.dart
@@ -1,43 +1,47 @@
+import 'package:first_project/main33/controller/product_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class ProductPage extends StatelessWidget {
-  const ProductPage({Key? key}) : super(key: key);
+  ProductPage({Key? key}) : super(key: key);
+  final ProductController productController = Get.put(ProductController());
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.black54,
-          elevation: 0,
-          leading: Icon(Icons.menu),
-          title: Text('shop'),
-          actions: [
-            Icon(Icons.transfer_within_a_station),
-            SizedBox(width: 15),
-            Icon(Icons.shopping_cart),
-          ],
-        ),
-        body: Scrollbar(
-          child: Container(
-            child: Padding(
-              padding: EdgeInsets.fromLTRB(0, 16, 0, 16),
-              child: GridView.builder(
+      appBar: AppBar(
+        backgroundColor: Colors.black54,
+        elevation: 0,
+        leading: Icon(Icons.menu),
+        title: Text('shop'),
+        actions: [
+          Icon(Icons.view_list_rounded),
+          SizedBox(width: 15),
+          Icon(Icons.shopping_cart),
+        ],
+      ),
+      body: Scrollbar(
+        child: Container(
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(0, 16, 0, 16),
+            child: Obx(
+              () => GridView.builder(
                 gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                   crossAxisCount: 2,
-                  childAspectRatio: 1 / 2, // 가로1, 세로2 == 0.5
-                  crossAxisSpacing: 10,
+                  childAspectRatio: 1, // 가로1, 세로2 == 0.5
+                  crossAxisSpacing: 5,
                   mainAxisSpacing: 10,
                 ),
                 itemBuilder: (context, index) {
-                  return Container(
-                    color: Colors.redAccent,
-                    child: Text('index: $index', style: TextStyle(fontSize: 24, color: Colors.white),),
-                  );
+                  // index 내려주고
+                  return Text('test');
                 },
-                itemCount: 8,
+                itemCount: productController.productList.length,
               ),
             ),
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
