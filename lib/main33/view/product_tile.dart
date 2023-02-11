bf427734bf80be71071bc1043a144b283fdb49f2
{krism01} <{krism01@naver.com}>
2023-02-12 00:42:59 +0900

    기능 구현 1. 각 컨테이너 라인 맞추기 위한 Stack에 Expanded 설정 2. 좋아요 실시간으로 변경한 내용 표시 3. Add to cart 기능 구현

--- lib/main33/view/product_tile.dart
+++ lib/main33/view/product_tile.dart
@@ -1,9 +1,12 @@
+import 'package:first_project/main33/controller/cart_controller.dart';
 import 'package:first_project/main33/model/product_model.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class ProductTile extends StatelessWidget {
   ProductTile(this.product);
   final Product product;
+  final cartController = Get.put(CartController());
 
   @override
   Widget build(BuildContext context) {
@@ -12,59 +15,81 @@ class ProductTile extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-          Stack(
-            children: [
-              Container(
-                height: 75,
-                width: 100,
-                clipBehavior: Clip.antiAlias,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(4),
+          Expanded(
+            child: Stack(
+              children: [
+                Container(
+                  height: 75,
+                  width: 100,
+                  clipBehavior: Clip.antiAlias,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(4),
+                  ),
+                  child: Image.network(
+                    product.imageLink,
+                    fit: BoxFit.fill,
+                  ),
                 ),
-                child: Image.network(
-                  product.imageLink,
-                  fit: BoxFit.fill,
+                Obx(
+                  () => CircleAvatar(
+                    backgroundColor: Colors.white,
+                    radius: 15,
+                    child: IconButton(
+                      icon: product.isFavorite.value
+                          ? Icon(Icons.favorite)
+                          : Icon(Icons.favorite_border_rounded),
+                      onPressed: () {
+                        product.isFavorite.toggle();
+                      },
+                      iconSize: 18,
+                    ),
+                  ),
                 ),
-              ),
-              CircleAvatar(
-                backgroundColor: Colors.white,
-                radius: 15,
-                child: IconButton(
-                  icon: Icon(Icons.favorite_rounded),
-                  onPressed: () {},
-                  iconSize: 18,
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
           SizedBox(height: 8),
           Text(
             product.name,
             maxLines: 2,
             style: TextStyle(fontWeight: FontWeight.w400),
-            overflow: TextOverflow.ellipsis,
+            overflow: TextOverflow.ellipsis, // ellipsis: 줄임표
           ),
           SizedBox(height: 8),
-          Container(
-            decoration: BoxDecoration(
-              color: Colors.green,
-              borderRadius: BorderRadius.circular(12),
-            ),
-            padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 2),
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text(
-                  product.rating != null? product.rating.toString() : '',
-                  style: TextStyle(color: Colors.white),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                  borderRadius: BorderRadius.circular(12),
                 ),
-                Icon(
-                  Icons.star,
-                  size: 16,
-                  color: Colors.white,
+                padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 2),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      product.rating != null ? product.rating.toString() : '',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    Icon(
+                      Icons.star,
+                      size: 16,
+                      color: Colors.white,
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  cartController.addToCart(product);
+                },
+                style: ElevatedButton.styleFrom(
+                  minimumSize: Size(20, 20),
+                ),
+                child: Text('Add to cart'),
+              ),
+            ],
           ),
           SizedBox(height: 8),
           Text(
