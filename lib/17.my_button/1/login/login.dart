f82d4408da4a7d76b8b6261c6edde48813a92d5d
{krism01} <{krism01@naver.com}>
2023-03-10 03:33:47 +0900

    폴더 변경에 따른 파일 이동 및 Google login setup

---
+++ lib/17.my_button/1/login/login.dart
@@ -0,0 +1,129 @@
+import 'package:first_project/17.my_button/1/button/button.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class LogIn extends StatefulWidget {
+  const LogIn({Key? key}) : super(key: key);
+
+  @override
+  State<LogIn> createState() => _LogInState();
+}
+
+class _LogInState extends State<LogIn> {
+  Future<UserCredential> signInWithGoogle() async {
+    // Trigger the authentication flow
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+    // test
+    if (googleUser != null) {
+      print('name = ${googleUser.displayName}');
+      print('email = ${googleUser.email}');
+      print('id = ${googleUser.id}');
+      // setState(() {});
+    }
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+    // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+    // Once signed in, return the UserCredential
+    return await FirebaseAuth.instance.signInWithCredential(credential);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: const Text('Sign in'),
+        centerTitle: true,
+        elevation: 0.2,
+      ),
+      // note: 간편하게 메서드를 아래로 분리
+      body: _buildContainer(),
+    );
+  }
+
+  Widget _buildContainer() {
+    return Builder(builder: (context) {
+      return Container(
+        padding: const EdgeInsets.all(30),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // note: 일반 버튼
+            ElevatedButton(
+              onPressed: () {
+                var msg = 'Google login 을 진행 중입니다.';
+                showSnackBarMsg(context, msg);
+
+                // note: Goolge login button
+                // _handleSignIn();
+                signInWithGoogle();
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(4.0)),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Image.asset('assets/main17/glogo.png'),
+                  const Text('Login with Google',
+                      style: TextStyle(color: Colors.black87, fontSize: 15.0)),
+                  Opacity(
+                      opacity: 0.0,
+                      child: Image.asset('assets/main17/glogo.png')),
+                ],
+              ),
+            ),
+            // note: 커스텀 위젯이며, button.dart 파일의 생성자에 들어갈 입력값 만들어서 새 버튼 만들기
+            MyButtons(
+              image: Image.asset('assets/main17/flogo.png'),
+              text: const Text('Login with Facebook',
+                  style: TextStyle(color: Colors.white)),
+              color: Colors.indigo,
+              onPressed: () {
+                var msg = 'Facebook login 을 진행 중입니다.';
+                showSnackBarMsg(context, msg);
+              },
+              radius: 8.0,
+            ),
+            // note: 일반 버튼
+            ElevatedButton(
+              onPressed: () {
+                var msg = 'login 을 진행 중입니다.';
+                showSnackBarMsg(context, msg);
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.green[600],
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  const Icon(Icons.email),
+                  const Text('Login with Email',
+                      style: TextStyle(color: Colors.white)),
+                  Opacity(
+                    opacity: 0.0,
+                    child: Image.asset('assets/main17/flogo.png'),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      );
+    });
+  }
+
+  void showSnackBarMsg(context, String msg) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text(msg)),
+    );
+  }
+}
