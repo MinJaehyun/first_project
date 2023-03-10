7d4f7a1752b1224c166838c07a5dc32507d20aad
{krism01} <{krism01@naver.com}>
2023-03-10 11:27:11 +0900

    google login 기능 추가

--- lib/17.my_button/1/login/login.dart
+++ lib/17.my_button/1/login/login.dart
@@ -1,4 +1,5 @@
 import 'package:first_project/17.my_button/1/button/button.dart';
+import 'package:first_project/17.my_button/1/google_page.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -11,6 +12,10 @@ class LogIn extends StatefulWidget {
 }
 
 class _LogInState extends State<LogIn> {
+  late String name;
+  late String email;
+  late String id;
+
   Future<UserCredential> signInWithGoogle() async {
     // Trigger the authentication flow
     final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
@@ -20,10 +25,18 @@ class _LogInState extends State<LogIn> {
       print('name = ${googleUser.displayName}');
       print('email = ${googleUser.email}');
       print('id = ${googleUser.id}');
-      // setState(() {});
+      setState(() {
+        name = googleUser.displayName!;
+        email = googleUser.email;
+        id = googleUser.id;
+      });
+      var googleLoginInfoMsg = ' name: $name\n email: $email\n id: $id';
+      showSnackBarMsg(context, googleLoginInfoMsg);
     }
+
     // Obtain the auth details from the request
-    final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+    final GoogleSignInAuthentication? googleAuth =
+        await googleUser?.authentication;
     // Create a new credential
     final credential = GoogleAuthProvider.credential(
       accessToken: googleAuth?.accessToken,
@@ -48,82 +61,89 @@ class _LogInState extends State<LogIn> {
   }
 
   Widget _buildContainer() {
-    return Builder(builder: (context) {
-      return Container(
-        padding: const EdgeInsets.all(30),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // note: 일반 버튼
-            ElevatedButton(
-              onPressed: () {
-                var msg = 'Google login 을 진행 중입니다.';
-                showSnackBarMsg(context, msg);
+    return Builder(
+      builder: (context) {
+        return Container(
+          padding: const EdgeInsets.all(30),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // note: 일반 버튼
+              ElevatedButton(
+                onPressed: () {
+                  signInWithGoogle();
+                  Future.delayed(Duration(seconds: 3));
 
-                // note: Goolge login button
-                // _handleSignIn();
-                signInWithGoogle();
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.white,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(4.0)),
+                  // fixme: 아래 테스트 중..
+                  // Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  //   return GooglePage(name: name, email: email, id: id);
+                  // },),);
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(4.0)),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Image.asset('assets/main17/glogo.png'),
+                    const Text('Login with Google',
+                        style:
+                            TextStyle(color: Colors.black87, fontSize: 15.0)),
+                    Opacity(
+                        opacity: 0.0,
+                        child: Image.asset('assets/main17/glogo.png')),
+                  ],
+                ),
               ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Image.asset('assets/main17/glogo.png'),
-                  const Text('Login with Google',
-                      style: TextStyle(color: Colors.black87, fontSize: 15.0)),
-                  Opacity(
+              // note: 커스텀 위젯이며, button.dart 파일의 생성자에 들어갈 입력값 만들어서 새 버튼 만들기
+              MyButtons(
+                image: Image.asset('assets/main17/flogo.png'),
+                text: const Text('Login with Facebook',
+                    style: TextStyle(color: Colors.white)),
+                color: Colors.indigo,
+                onPressed: () {
+                  var msg = 'Facebook login 을 진행 중입니다.';
+                  showSnackBarMsg(context, msg);
+                },
+                radius: 8.0,
+              ),
+              // note: 일반 버튼
+              ElevatedButton(
+                onPressed: () {
+                  var msg = 'login 을 진행 중입니다.';
+                  showSnackBarMsg(context, msg);
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.green[600],
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    const Icon(Icons.email),
+                    const Text('Login with Email',
+                        style: TextStyle(color: Colors.white)),
+                    Opacity(
                       opacity: 0.0,
-                      child: Image.asset('assets/main17/glogo.png')),
-                ],
+                      child: Image.asset('assets/main17/flogo.png'),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            // note: 커스텀 위젯이며, button.dart 파일의 생성자에 들어갈 입력값 만들어서 새 버튼 만들기
-            MyButtons(
-              image: Image.asset('assets/main17/flogo.png'),
-              text: const Text('Login with Facebook',
-                  style: TextStyle(color: Colors.white)),
-              color: Colors.indigo,
-              onPressed: () {
-                var msg = 'Facebook login 을 진행 중입니다.';
-                showSnackBarMsg(context, msg);
-              },
-              radius: 8.0,
-            ),
-            // note: 일반 버튼
-            ElevatedButton(
-              onPressed: () {
-                var msg = 'login 을 진행 중입니다.';
-                showSnackBarMsg(context, msg);
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.green[600],
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  const Icon(Icons.email),
-                  const Text('Login with Email',
-                      style: TextStyle(color: Colors.white)),
-                  Opacity(
-                    opacity: 0.0,
-                    child: Image.asset('assets/main17/flogo.png'),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      );
-    });
+            ],
+          ),
+        );
+      },
+    );
   }
 
   void showSnackBarMsg(context, String msg) {
     ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text(msg)),
+      SnackBar(
+        content: Text(msg),
+        backgroundColor: Colors.blue,
+      ),
     );
   }
 }
