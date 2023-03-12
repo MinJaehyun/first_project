5223c80dbd0331cea2ff2b95601469cbdd25d341
{krism01} <{krism01@naver.com}>
2023-03-13 00:48:11 +0900

    AsyncMemoizer 사용하여 FutureBuilder 내 builder 가 rebuild 되지 않게 설정

--- lib/main20/main20-1/main20.dart
+++ lib/main20/main20-1/main20.dart
@@ -1,3 +1,4 @@
+import 'package:async/async.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -26,10 +27,12 @@ class _MyPageState extends State<MyPage> {
   String currentData = 'no data found';
   String bottomData = ' 버튼을 사용하지 않고,\n 데이터를 읽어 오는 중입니다';
   var style = const TextStyle(fontSize: 18, color: Colors.redAccent);
+  late AsyncMemoizer _memoizer;
 
   @override
   void initState() {
     super.initState();
+    _memoizer = AsyncMemoizer();
   }
 
   @override
@@ -54,28 +57,34 @@ class _MyPageState extends State<MyPage> {
 
             // note: 실시간으로 데이터를 가져와서 변경하는 방법
             FutureBuilder(
-
               // note: 아..착각했다 future 에는 return 될 결과를 작성해야 한다!
-              // note: 초기값 설정은 initialData, ConnectionState.waiting 설정 안 되였으면 이 초기값을 나타낸다
-              // initialData: "Initial Data",
-              initialData: bottomData,
-
+              // note: 초기값 설정은 initialData, ConnectionState.waiting 설정하거나 이 초기값 둘 중 하나를 설정한다
+              // initialData: bottomData,
               // note: future: 에는 done 에서 사용할 마지막 출력할 값을 설정한다
               future: myData(),
               builder: (context, snapshot) {
-                print('FutureBuilder re builder');
-                if (snapshot.connectionState == ConnectionState.none) {
-                  // note: done 언제 사용? 앱이 초기화 중이거나, 네트워크 연결이 끊기거나, 데이터 로딩 전일 때.
-                  return Text('앱이 초기화 중이거나, 네트워크 연결이 끊기거나, 데이터를 로딩 전 입니다.');
-                }
+                // print('FutureBuilder start'); // 순서 1, 3, 5
+                // if (snapshot.connectionState == ConnectionState.none) {
+                //   print('FutureBuilder none'); // none 상태 아니므로 pass
+                //   // note: done 언제 사용? 앱이 초기화 중이거나, 네트워크 연결이 끊기거나, 데이터 로딩 전일 때.
+                //   return Text('앱이 초기화 중이거나, 네트워크 연결이 끊기거나, 데이터를 로딩 전 입니다.');
+                // }
                 // note: waiting 는 연결 중이므로 대기중인 문자열이나 로딩을 출력한다
-                else if (snapshot.connectionState == ConnectionState.waiting) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  print('FutureBuilder waiting'); // 순서 2, 4
                   return Column(
-                    children: [Text(bottomData, style: style), CircularProgressIndicator()],
+                    children: [
+                      Text(bottomData, style: TextStyle(fontSize: 25)),
+                      CircularProgressIndicator()
+                    ],
                   );
                 }
+
                 // note: ConnectionState.done 상태이며, future: myData() 실행값은 snapshot.data 에 담겨있다
-                return Text(snapshot.data!, style: TextStyle(fontSize: 25));
+                // else {
+                  print('FutureBuilder done'); // 순서 6
+                  return Text(snapshot.data!, style: TextStyle(fontSize: 25));
+                // }
               },
             )
           ],
@@ -102,8 +111,18 @@ class _MyPageState extends State<MyPage> {
     });
   }
 
-  Future<String> myData() async {
-    await Future.delayed(const Duration(seconds: 2));
-    return 'The data is fetched';
+  // Future<String> myData() async {
+  //   await Future.delayed(Duration(seconds: 2));
+  //   return 'The data is fetched';
+  // }
+
+  // setstate 할 때 해당 함수는 캐쉬에 저장되어 있는 값을 바로 리턴하므로,
+  // 결국 async 함수 안에 코드는 실행 하지 않고 캐쉬에 저장한 결과값만 리턴한다.
+  Future<dynamic> myData() async {
+    return this._memoizer.runOnce(() async {
+      await Future.delayed(Duration(seconds: 2));
+      return 'The data is fetched';
+    });
   }
 }
+
