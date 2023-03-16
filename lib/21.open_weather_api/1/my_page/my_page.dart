c1dcaecab0c97beea570ceeabdd49eb62675b6a9
{krism01} <{krism01@naver.com}>
2023-03-16 18:33:54 +0900

    개선 1. 위도와 경도는 앱실행 즉시 가져오고 2. 날씨 정보 가져오기는 지역의 정보를 가져와서 날씨를 이미지와 정보로 나타내기 위한 데이터이다

--- lib/21.open_weather_api/1/my_page/my_page.dart
+++ lib/21.open_weather_api/1/my_page/my_page.dart
@@ -12,52 +12,63 @@ class MyPage extends StatefulWidget {
 
 class _MyPageState extends State<MyPage> {
   Position? position;
-  http.Response? response;
+  var myJson;
 
-  void geoLocation() async {
+  Future<void> geoLocation() async {
+    // todo: 추 후 helper/network.dart 로 분리하기
     try {
       await Geolocator.requestPermission();
       position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-      debugPrint(position.toString());
     } catch (e) {
       debugPrint('internet connection err');
     }
   }
 
-  Future<String?> fetchData() async {
+  // todo: 추 후 helper/api_fetchData.dart 로 분리하기
+  Future<void> fetchData() async {
     var url = Uri.parse('https://samples.openweathermap.org/data/2.5/weather?q=London&appid=b1b15e88fa797225412429c1c50c122a1');
-    response = await http.get(url);
-    String? jsonData = response?.body;
-    var myJson = jsonDecode(jsonData!)['weather'][0];
-    debugPrint(myJson);
-    return myJson;
+    http.Response? response = await http.get(url);
+    var jsonData = response.body;
+    setState(() {
+      myJson = jsonDecode(jsonData)['weather'][0];
+      // {id: 300, main: Drizzle, description: light intensity drizzle, icon: 09d}
+    });
   }
 
   @override
   void initState() {
     super.initState();
     geoLocation();
-    fetchData();
   }
 
   @override
   Widget build(BuildContext context) {
-    debugPrint('build start');
+    var style = TextStyle(fontSize: 25);
     return Scaffold(
       appBar: AppBar(title: const Text('MyPage'), centerTitle: true),
       body: Center(
         child: SingleChildScrollView(
           child: Column(
             children: [
-              Text(position.toString(), style: const TextStyle(fontSize: 35)),
+              Text('앱 실행 시, 위도와 경도 나타내기', style: TextStyle(fontSize: 25)),
+              if (position != null) Text(position!.toString(), style: const TextStyle(fontSize: 35)),
               const Divider(height: 20, thickness: 3),
+              Text(' 지역 날씨 정보', style: TextStyle(fontSize: 25)),
               ElevatedButton(
-                  onPressed: () {
-                    fetchData();
-                  },
-                  child: const Text('http get')),
-              Text((response?.body).toString(), style: const TextStyle(fontSize: 35)),
-              const Divider(height: 20, thickness: 3),
+                onPressed: () {
+                  fetchData();
+                },
+                child: const Text('날씨 정보 가져오기'),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  if (myJson != null) Text('id: ${myJson['id'].toString()}', style: style),
+                  if (myJson != null) Text('main: $myJson["main"]', style: style),
+                  if (myJson != null) Text('description: $myJson["description"]', style: style),
+                  if (myJson != null) Text('iconNumber: $myJson["icon"]', style: style),
+                ],
+              ),
             ],
           ),
         ),
