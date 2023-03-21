503fb43553ca1e7fc28c27abea2021238f1eb86e
{krism01} <{krism01@naver.com}>
2023-03-21 13:09:59 +0900

    주석 첨부

--- lib/main24/main24.dart
+++ lib/main24/main24.dart
@@ -1,8 +1,8 @@
+import 'package:first_project/main24/screen/chat/chat_screen.dart';
+import 'package:first_project/main24/screen/signin_signup/signin_signup.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'chat_screen/chat_screen.dart';
-import '../main24/my_page/my_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,13 +17,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      // note: ★ StreamBuilder 에서 authStateChanges 으로 토큰유무 체크하고 진행하는게 특징
       home: StreamBuilder(
         stream: FirebaseAuth.instance.authStateChanges(),
         builder: ((context, snapshot) {
           if (snapshot.hasData) {
             return const ChatScreen();
           }
-          return const MyPage();
+          return const SigninSignup();
         }),
       ),
     );
