b12b80e1cb93267f402ac200f6d893edd7943961
{krism01} <{krism01@naver.com}>
2023-03-23 00:00:35 +0900

    개선 1. StreamBuilder 를 하위로 보내어 분리해서 사용 2. 메시지를 입력받을 위젯 설정

--- lib/24.chat_test/screen/chat/chat_screen.dart
+++ lib/24.chat_test/screen/chat/chat_screen.dart
@@ -1,5 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:first_project/24.chat_test/screen/chat/message.dart';
+import 'package:first_project/24.chat_test/screen/chat/new_message.dart';
 import 'package:first_project/24.chat_test/screen/signin_signup/signin_signup.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
@@ -55,28 +57,17 @@ class _ChatScreenState extends State<ChatScreen> {
         ],
       ),
       // todo: 직접 Firestore 웹사이트에 접근하여 컬렉션 및 문서 및 필드를 작성하였는데, 코드를 통해 message를 몇 개 추가해보기. add? set?
-      body: StreamBuilder(
-        stream: FirebaseFirestore.instance.collection('/chats/pZaVfyO0YlsbF0VMyTLL/messages').snapshots(),
-        builder: (context, AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          }
-          final docs = snapshot.data!.docs;
-          return ListView.builder(
-            itemCount: docs.length,
-            itemBuilder: (BuildContext context, int index) {
-              return Container(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  children: [
-                    Text(docs[index]['text'], style: TextStyle(fontSize: 25)),
-                    Divider(thickness: 3),
-                  ],
-                ),
-              );
-            },
-          );
-        },
+      body: Container(
+        child: Center(
+          child: Column(
+            children: [
+              // note: 컬럼위젯 내에 리스트뷰가 무조건 화면 내의 모든 공간을 차지하므로, Expanded 설정함
+              // note: Message 내에서 NewMessage()까지 처리하면 안된다
+              Expanded(child: Message()),
+              NewMessage(),
+            ],
+          ),
+        ),
       ),
     );
   }
