8b68d969af1cce491fbad7019382ed6be52cde7e
MinJaehyun <43669992+MinJaehyun@users.noreply.github.com>
2023-06-14 12:35:31 +0900

    기능 오류 해결

--- lib/main20/README.md
+++ lib/main20/README.md
@@ -1,21 +1,20 @@
-### 결과물
+### 결과물 1. - old
 <img src="https://user-images.githubusercontent.com/43669992/224541206-409e8ed4-d226-4f6e-b7b4-2e247da9f559.gif" width="200" height="400"/>
 
-### 1번 project 학습 내용
-    1. 버튼을 사용하여 데이터를 가져오는 방법
-    2. 버튼을 사용하지 않고 데이터를 가져오는 방법
-    3. FutureBuilder 사용법
-    4. snapshot.ConnectionState 의 none, waiting, done 의 차이점
-    5. 함수 실행 결과를 받아서 처리하는 2가지 방법
+### 결과물 2. - 기능 이해 및 오류 수정
+<img src="https://github.com/MinJaehyun/first_project/assets/43669992/1f600996-6642-40ae-b557-37f88e0196cb" width="200" height="400"/>
 
-### 2번 project 학습 내용
+### 1번 project
+    1. 버튼 클릭하여 지연된 데이터 가져오는 방법
+    2. 버튼 클릭하지 않고 지연된 데이터를 가져오는 방법
+    3. FutureBuilder 사용법
+    4. snapshot.ConnectionState 종류(none, waiting, done)와 snapshot.hasData 의 차이점
+    5. 함수 실행 결과 받는 2가지 방법
+
+### 2번 project
     1. snapshot.hasData 사용하여 처리하는 방법
 
-### 느낀점
-    * snapshot.ConnectionState 는 현재 상태를 나타내는 반면, snapshot.hasData 는 데이터가 성공적으로 반환 되었는지 여부를 나타낸다
-    * 근데 동일한 결과는 나타내므로 아직까지 차이를 못느끼겠다..
-    * snapshot.ConnectionState 사용할 경우, 버튼을 클릭하면 같이 데이터가 변경된다
-    * snapshot.hasData 사용할 경우, 버튼을 클릭하면 아래는 데이터가 변경되자 않는다
-    * 왜 ? snapshot.connectionState == ConnectionState.waiting 처리 시, re builder 되고 있다
-    * 일단, FutureBuilder rebuilder 안되게 하려면 snapshot.hasData 사용하거나,   
-    * ConnectionState.waiting 사용 시, async 설치 및 return this._memoizer.runOnce(() async {} 설정한다.  
+### 정리: snapshot.ConnectionState 와 snapshot.hasData 차이
+    1. 네트워크를 통해 데이터를 가져와 사용할 땐 ConnectionState 사용하고, 
+       네트워크를 통해 데이터를 가져오지 않을 땐 hasData 사용한다
+    2. FutureBuilder 내 future 에는 화면에 출력할 결과값을 가져오고, 이를 snapShot.data 로 처리한다 
--- lib/main20/main20-1/main20.dart
+++ lib/main20/main20-1/main20.dart
@@ -1,4 +1,3 @@
-import 'package:async/async.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -25,66 +24,32 @@ class MyPage extends StatefulWidget {
 
 class _MyPageState extends State<MyPage> {
   String currentData = 'no data found';
-  String bottomData = ' 버튼을 사용하지 않고,\n 데이터를 읽어 오는 중입니다';
-  var style = const TextStyle(fontSize: 18, color: Colors.redAccent);
-  late AsyncMemoizer _memoizer;
-
-  @override
-  void initState() {
-    super.initState();
-    _memoizer = AsyncMemoizer();
-  }
+  TextStyle style = const TextStyle(fontSize: 26, color: Colors.blueAccent);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Future Test')),
+      appBar: AppBar(title: const Text('Future Test'), centerTitle: true),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            // note: 버튼을 클릭하여 데이터를 가져와서 변경하는 방법
-            ElevatedButton(
-              onPressed: () {
-                fetchData();
-              },
-              child: const Text('Future btn'),
-            ),
-            const SizedBox(height: 22),
             Text(currentData, style: style),
+            const SizedBox(height: 22),
+            // note: 버튼 클릭하여 데이터 가져와서 변경
+            ElevatedButton(onPressed: () => fetchData(), child: const Text('데이터 가져오기')),
             const Divider(height: 20, thickness: 3.5, indent: 55, endIndent: 55),
             const SizedBox(height: 22),
 
-            // note: 실시간으로 데이터를 가져와서 변경하는 방법
+            // note: 실시간 데이터 가져와 변경
             FutureBuilder(
-              // note: 아..착각했다 future 에는 return 될 결과를 작성해야 한다!
-              // note: 초기값 설정은 initialData, ConnectionState.waiting 설정하거나 이 초기값 둘 중 하나를 설정한다
-              // initialData: bottomData,
-              // note: future: 에는 done 에서 사용할 마지막 출력할 값을 설정한다
+              // note: future 에는 done 에서 사용할 마지막 출력할 값을 설정한다
               future: myData(),
               builder: (context, snapshot) {
-                // print('FutureBuilder start'); // 순서 1, 3, 5
-                // if (snapshot.connectionState == ConnectionState.none) {
-                //   print('FutureBuilder none'); // none 상태 아니므로 pass
-                //   // note: done 언제 사용? 앱이 초기화 중이거나, 네트워크 연결이 끊기거나, 데이터 로딩 전일 때.
-                //   return Text('앱이 초기화 중이거나, 네트워크 연결이 끊기거나, 데이터를 로딩 전 입니다.');
-                // }
-                // note: waiting 는 연결 중이므로 대기중인 문자열이나 로딩을 출력한다
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  print('FutureBuilder waiting'); // 순서 2, 4
-                  return Column(
-                    children: [
-                      Text(bottomData, style: TextStyle(fontSize: 25)),
-                      CircularProgressIndicator()
-                    ],
-                  );
+                if (snapshot.hasData) {
+                  return Column(children: [Text(snapshot.data.toString(), style: TextStyle(fontSize: 25))]);
                 }
-
-                // note: ConnectionState.done 상태이며, future: myData() 실행값은 snapshot.data 에 담겨있다
-                // else {
-                  print('FutureBuilder done'); // 순서 6
-                  return Text(snapshot.data!, style: TextStyle(fontSize: 25));
-                // }
+                return CircularProgressIndicator();
               },
             )
           ],
@@ -95,34 +60,25 @@ class _MyPageState extends State<MyPage> {
 
   Future<void> fetchData() async {
     Duration duration = const Duration(seconds: 2);
-    // 방법 1. Future.delayed()
+    // 방법 1. Future.delayed() 의 두 번째 인자인 함수를 활용하는 방법
+    // note: factory Future.delayed(Duration duration, [FutureOr<T> computation()?]) {
     // await Future.delayed(duration, () {
     //   setState(() {
     //     currentData = 'The data is fetched';
     //   });
     // });
 
-    // 방법 2. Future실행 성공 후, .then 이하 코드 실행하는 방법
+    // 방법 2. Future 실행 후, .then 으로 결과 받는 방법
     await Future.delayed(duration).then((val) {
       setState(() {
-        currentData = 'The data is fetched';
-        style = const TextStyle(fontSize: 22, color: Colors.green);
+        currentData = '버튼을 사용하여\n데이터를 가져 왔습니다';
       });
     });
   }
 
-  // Future<String> myData() async {
-  //   await Future.delayed(Duration(seconds: 2));
-  //   return 'The data is fetched';
-  // }
-
-  // setstate 할 때 해당 함수는 캐쉬에 저장되어 있는 값을 바로 리턴하므로,
-  // 결국 async 함수 안에 코드는 실행 하지 않고 캐쉬에 저장한 결과값만 리턴한다.
-  Future<dynamic> myData() async {
-    return this._memoizer.runOnce(() async {
-      await Future.delayed(Duration(seconds: 2));
-      return 'The data is fetched';
-    });
+  Future<String> myData() async {
+    Duration duration = const Duration(seconds: 4);
+    await Future.delayed(duration);
+    return '버튼을 사용하지 않고\n데이터를 가져 왔습니다';
   }
 }
-
